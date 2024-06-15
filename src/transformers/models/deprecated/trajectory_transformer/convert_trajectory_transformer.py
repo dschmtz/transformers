a694767956ecc3eeb86ed7ccc8a0df0046daeb6f
Daniel Schmitz <96418346+dschmtz@users.noreply.github.com>
2024-06-15 18:15:03 +0200

    Avoid path limit

--- src/transformers/models/deprecated/trajectory_transformer/convert_trajectory_transformer.py
+++ src/transformers/models/deprecated/trajectory_transformer/convert_trajectory_transformer.py
@@ -12,6 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# ORIGINAL FILE NAME
+# convert_trajectory_transformer_original_pytorch_checkpoint_to_pytorch.py
 """TrajectoryTransformer pytorch checkpoint conversion"""
 
 import torch
